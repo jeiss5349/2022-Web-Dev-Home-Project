12563b9d35d3326912f3c43c479e98f666abbd75
jeiss5349 <varghesj4@newpaltz.edu>
2022-10-21 02:48:58 -0400

    Added table and stuff for admin page

--- client/src/stores/session.ts
+++ client/src/stores/session.ts
@@ -3,8 +3,16 @@ import { reactive } from "vue";
 
 const session = reactive( {
     user: null as User | null,
+    users: [] as User[],
 });
-
+export function userAdd(firstName: string, lastName: string, adminUser: boolean)
+{
+    if(!session.users.find((u) => u.firstName === firstName && u.lastName === lastName))
+    {
+        const user = new User(firstName, lastName, adminUser);
+        session.users.push(user);
+    }
+}
 export function login(firstName: string, lastName: string, adminUser: boolean) {
     session.user = {
         firstName,
@@ -21,6 +29,13 @@ export class User {
     public firstName?: string;
     public lastName?: string;
     public adminUser?: boolean;
+
+    constructor(firstName: string,lastName: string, adminUser: boolean)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adminUser = adminUser;
+      }
 }
 
 
--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -6,6 +6,24 @@
         <button class="button is-medium is-primary">Add</button>
         <button class="button is-medium is-link">Edit</button>
       </div>
+      <div class="table-container column is-6 mx-auto">
+      <table class="table is-fullwidth mt-6">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Is Admin</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in session.users">
+            <td>{{ user.firstName }}</td>
+            <td>{{ user.lastName }}</td>
+            <td>{{ user.adminUser }}</td>
+          </tr>
+        </tbody>  
+      </table>
+     </div> 
   </main>
   <main v-else>
     <h1 class="title">You are not an admin</h1>
